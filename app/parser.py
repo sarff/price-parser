05ca950da0772fe51ef3133edc24c148e212798d
itdim <sarff@mail.ru>
2021-06-18 10:57:15 +0300

    replacestr to loop

--- app/parser.py
+++ app/parser.py
@@ -14,7 +14,12 @@ class Parce():
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'lxml')
         quotes = soup.find_all('table', class_='table_price')
-        table = ' '.join(map(str, quotes)).replace(replacestr,"").replace("'я",r"\'я").replace("'є",r"\'є")
+        table = ' '.join(map(str, quotes))
+        for i in range(len(replacestr)):
+            if replacestr[i].find("'") == -1:
+                table = table.replace(replacestr[i], "")
+            else:
+                table = table.replace(replacestr[i], r"\%s" % (replacestr[i]))
         add_date = "<p>Дата прайсу: %s </p>" % datetime.date.today()
         fulltable = "%s %s" % (add_date, table)
         return fulltable
