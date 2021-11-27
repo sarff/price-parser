0b58313b9fefc0c436ad780470b2a4bdfa173828
itdim <sarff@mail.ru>
2021-11-27 18:44:13 +0200

    fix read code

--- app/database.py
+++ app/database.py
@@ -5,6 +5,8 @@ config = toml.load("conf/config.toml")
 #config = toml.load("/root/priceupdate/price-parser/conf/config.toml")
 updtable = config.get('dbtable').get('updtable')
 updrow = config.get('dbtable').get('updrow')
+updwhere = config.get('dbtable').get('updwhere')
+updwhere2 = config.get('dbtable').get('updwhere2')
 
 
 
@@ -17,10 +19,24 @@ mydb = mysql.connector.connect(
 
 class MySql():
     @staticmethod
-    def update_price(setvalue,updwhere):
+    def update_price(setvalue):
         try:
             mycursor = mydb.cursor()
             sql = "UPDATE {0} SET {1} = '{2}' WHERE id = {3} LIMIT 1".format(updtable,updrow,setvalue,updwhere)
+            mycursor.execute(sql)
+            mydb.commit()
+            mycursor.close()
+
+            return True
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+    @staticmethod
+    def update_upd(setvalue):
+        try:
+            mycursor = mydb.cursor()
+            sql = "UPDATE {0} SET {1} = '{2}' WHERE id = {3} LIMIT 1".format(updtable, updrow, setvalue, updwhere2)
             mycursor.execute(sql)
             mydb.commit()
             mycursor.close()
--- parce_price.py
+++ parce_price.py
@@ -3,12 +3,7 @@ from app.parser import Parce
 import toml
 
 if __name__ == '__main__':
-    config = toml.load("conf/config.toml")
-    # config = toml.load("/root/priceupdate/price-parser/conf/config.toml")
-    updwhere = config.get('dbtable').get('updwhere')
-    updwhere2 = config.get('dbtable').get('updwhere2')
-
     newtable = Parce.returntable()
-    MySql.update_price(newtable, updwhere)
+    MySql.update_price(newtable)
     newtable2 = Parce.returntable_upd()
-    MySql.update_price(newtable2, updwhere2)
+    MySql.update_upd(newtable2)
